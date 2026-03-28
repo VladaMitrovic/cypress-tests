6f7e25450017312779d7fb238187736af0410238
vladimirakenza <152073352+vladimirakenza@users.noreply.github.com>
2026-03-28 16:35:23 +0100

    update test 2

--- tests/2-network.cy.ts
+++ tests/2-network.cy.ts
@@ -1,5 +1,7 @@
-describe('Task 2 - Network Validation', () => {
-  it('validates inventory page response after login', () => {
+describe('Task 2 - Network Interception', () => {
+  it('intercepts a relevant product page request and validates response', () => {
+    cy.intercept('GET', '**/*.js').as('assetRequest')
+
     cy.visit('/')
 
     cy.get('[data-test="username"]').type(Cypress.env('USERNAME')!)
@@ -8,10 +10,11 @@ describe('Task 2 - Network Validation', () => {
 
     cy.url().should('include', '/inventory.html')
 
-    cy.request('/inventory.html').then((response) => {
-      expect(response.status).to.eq(200)
-      expect(response.headers).to.have.property('content-type')
-      expect(response.body).to.include('inventory_list')
+    cy.wait('@assetRequest').then((interception) => {
+      expect(interception.response?.statusCode).to.eq(200)
+      expect(interception.response?.headers).to.have.property('content-type')
+      expect(interception.response?.headers['content-type']).to.include('javascript')
+      expect(interception.request).to.have.property('url')
     })
   })
 })
